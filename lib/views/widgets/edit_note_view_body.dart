8f98d0c084568e434f13cb034522385872fd6e4c
karimkhater <kimokhater137@gmail.com>
2026-08-14 04:36:09 +0300

    feat: added Hive packages & created note model and type adapter

--- lib/views/widgets/edit_note_view_body.dart
+++ lib/views/widgets/edit_note_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app_project/views/widgets/custom_app_bar.dart';
+import 'package:notes_app_project/views/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -11,8 +12,15 @@ class EditNoteViewBody extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12),
           child: Column(
+            spacing: 20,
             children: [
-              CustomAppBar(title: "Edit Note", icon: Icons.check, onPressed: (){},)
+              CustomAppBar(title: "Edit Note", icon: Icons.check, onPressed: (){},),
+
+              CustomTextField(hint: "Title"),
+
+              CustomTextField(hint: "Content", maxLines: 5,),
+
+
             ],
           ),
         ),
